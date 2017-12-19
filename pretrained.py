c74293939573eb04edc5022b0d145187d3938f16
Paulo Viadanna <paulo@viadanna.net>
2017-12-19 14:07:24 -0200

    Third revision
    Replace README.md
    Fine tuning

--- pretrained.py
+++ pretrained.py
@@ -4,20 +4,12 @@ from keras.applications.vgg16 import VGG16
 from keras.applications.mobilenet import MobileNet
 
 
-def resize(image, size=(160, 160)):
-    from keras.backend import tf
-    return tf.image.resize_images(image, size)
-
-
-def get_mobilenet(image, velocity):
+def get_vgg16(image, velocity):
     # Inputs
     image = Cropping2D(cropping=((60, 20), (0, 0)))(image)
-    image = Lambda(resize)(image)
 
-    # Load pretrained MobileNet
-    base_model = MobileNet(
-        input_shape=(160, 160, 3),
-        input_tensor=image, weights='imagenet', include_top=False)
+    # Load pretrained VGG16
+    base_model = VGG16(input_tensor=image, weights='imagenet', include_top=False)
     for layer in base_model.layers:
         layer.trainable = False
 
@@ -43,28 +35,35 @@ def get_mobilenet(image, velocity):
     return model
 
 
-def get_vgg16(image, velocity):
+def resize(image, size=(160, 160)):
+    from keras.backend import tf
+    return tf.image.resize_images(image, size)
+
+
+def get_mobilenet(image, velocity):
     # Inputs
     image = Cropping2D(cropping=((60, 20), (0, 0)))(image)
+    image = Lambda(resize)(image)
 
-    # Load pretrained VGG16
-    base_model = VGG16(input_tensor=image, weights='imagenet', include_top=False)
+    # Load pretrained MobileNet
+    base_model = MobileNet(
+        input_shape=(160, 160, 3),
+        input_tensor=image, weights='imagenet', include_top=False)
     for layer in base_model.layers:
         layer.trainable = False
 
     # Flattening for fully-connected
     x = base_model.output
     x = Flatten()(x)
-    x = Dropout(.25)(x)
-    x = Dense(100, activation='relu')(x)
 
     # Steering tower
-    xa = Dense(50, activation='relu')(x)
-    xa = Dense(10, activation='relu')(xa)
+    xa = Dense(100, activation='elu')(x)
+    xa = Dense(50, activation='elu')(xa)
+    xa = Dense(10, activation='elu')(xa)
     xa = Dense(1, name='steering')(xa)
 
     # Throttle tower
-    xt = concatenate([x, xa, velocity])
+    xt = concatenate([x, velocity])
     xt = Dropout(.5)(xt)
     xt = Dense(10, activation='relu')(xt)
     xt = Dense(1, name='throttle')(xt)
